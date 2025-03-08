08eb172e155119af7807f5640099c260c3d88085
Ajinkya <ajinkyagorad@users.noreply.github.com>
2025-03-08 06:44:50 +0200

    Add files via upload

---
+++ hardware/SO107_urdf_description/hello_bullet.py
@@ -0,0 +1,25 @@
+
+import pybullet as p
+import os
+import time
+import pybullet_data
+physicsClient = p.connect(p.GUI)#or p.DIRECT for non-graphical version
+p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
+p.setGravity(0,0,-10)
+planeId = p.loadURDF("plane.urdf")
+cubeStartPos = [0,0,0]
+cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
+dir = os.path.abspath(os.path.dirname(__file__))
+robot_urdf = "SO107_urdf.urdf"
+dir = os.path.join(dir,'urdf')
+robot_urdf=os.path.join(dir,robot_urdf)
+robotId = p.loadURDF(robot_urdf,cubeStartPos, cubeStartOrientation, 
+                   # useMaximalCoordinates=1, ## New feature in Pybullet
+                   flags=p.URDF_USE_INERTIA_FROM_FILE)
+for i in range (10000):
+    p.stepSimulation()
+    time.sleep(1./240.)
+cubePos, cubeOrn = p.getBasePositionAndOrientation(robotId)
+print(cubePos,cubeOrn)
+p.disconnect()
+
